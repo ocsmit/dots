9db830ed41f3683d60c441413a63d6d6884ac8fd
ocsmit <osmith2@ncsu.edu>
2022-04-17 14:39:20 -0400

    Update install

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ DIR="${PWD}/config"
 
 ln -s $DIR/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -s $DIR/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -s $DIR/UltiSnips $HOME/.config/UltiSnips
+ln -s $DIR/.tmux.conf $HOME/.tmux.conf
