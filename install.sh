817ea1d3bbc137d2c3d45c5bafbc8c6043b30326
ocsmit <osmith2@ncsu.edu>
2022-04-17 12:52:35 -0400

    Migrate to config

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ln -s $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+DIR="${PWD}/config"
+
+ln -s $DIR/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $DIR/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
