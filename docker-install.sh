c6a3fc0c280210c16596edca13ecea2d7b3efbbf
ocsmit <osmith2@ncsu.edu>
2022-06-09 19:04:51 +0000

    Update

--- docker-install.sh
+++ docker-install.sh
@@ -3,7 +3,21 @@
 apt-get update
 
 apt install -y \
-    neovim \
-    tmux
+    tmux \
+    ripgrep 
+
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+
+sudo dpkg -i --force-overwrite nvim-linux64.deb
+
+pip install black
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir -p $HOME/.config 
+mkdir -p $HOME/.config/nvim
 
 ./install.sh
+
+
