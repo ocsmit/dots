eb6eb9d4be09f83b56542e232039ee01f2890a26
ocsmit <osmith2@ncsu.edu>
2022-06-24 15:42:15 +0000

    Update docker installer

--- docker-install.sh
+++ docker-install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
+# Install requirements
 apt-get update
-
 apt install -y \
     tmux \
-    ripgrep 
+    ripgrep \
+    zsh
 
-wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+# Get latest nvim 0.7.0 + plugins
+wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb -O nvim.deb
 
-sudo dpkg -i --force-overwrite nvim-linux64.deb
+sudo dpkg -i --force-overwrite nvim.deb
 
 pip install black
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-mkdir -p $HOME/.config 
+python3 -m pip install --user --upgrade pynvim
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# symlink config files
+mkdir -p $HOME/.config
 mkdir -p $HOME/.config/nvim
 
+echo 'alias vi="nvim"' >> ~/.zshrc
+source ~/.zshrc
+
 ./install.sh
-
-
