d7755e87df9b912ea07f044fa9a8defffbcadad7
ocsmit <osmith2@ncsu.edu>
2022-07-04 14:00:46 -0400

    zsh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR="${PWD}/config"
 
+ln -sf $DIR/.zshrc $HOME/.zshrc
 ln -sf $DIR/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $DIR/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 ln -sf $DIR/UltiSnips $HOME/.config/UltiSnips
 ln -sf $DIR/.tmux.conf $HOME/.tmux.conf
